2f45cce423c0d105e104f649a05b20d59cfd0507
Nathan Monte <nathan.monte.kh@gmail.com>
2017-02-18 14:46:28 +0000

    File upload logic completed using multer for multipart forms

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var multer = require("multer");
+var upload = multer({ dest: 'uploads/' });
+
+//Body parser is needed to read values from submitted forms
+var bodyParser = require("body-parser");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+//Setup for public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser is needed to read values from submitted forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log('Example app listening on port: ' + port + '!');
+})
+
+/* GET home page. */
+app.get("/", function(req, res, next) {
+    res.render("index", {});
+})
+
+//Page with form on it.
+app.get("/submit", function(req, res, next) {
+    res.render("submit", {});
+})
+
+app.post("/submit", upload.single("uploaded_file"), function(req, res, next) {
+    "use strict";
+    let return_json = {};
+    if (req.file) {
+        return_json.size = req.file["size"];
+    } else {
+        return_json.error = "Please provide a file.";
+    }
+    res.json(return_json);
+})
